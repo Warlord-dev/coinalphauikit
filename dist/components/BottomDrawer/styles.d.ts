3a1e5dbdc3fc5f99796ed1b47fb34d58b50651d7
Warlord <brightness0719@gmail.com>
2021-11-24 08:16:48 -0800

    update

---
+++ dist/components/BottomDrawer/styles.d.ts
@@ -0,0 +1,4 @@
+export declare const DrawerContainer: import("styled-components").StyledComponent<"div", import("styled-components").DefaultTheme, {
+    isUnmounting: boolean;
+}, never>;
+export declare const StyledOverlay: import("styled-components").StyledComponent<"div", import("styled-components").DefaultTheme, {}, never>;
