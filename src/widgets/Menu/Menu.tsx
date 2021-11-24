dd3afcc24d7ab5f50a7d48153c4e5ef57c959742
Warlord <brightness0719@gmail.com>
2021-11-24 09:59:44 -0800

    update

--- src/widgets/Menu/Menu.tsx
+++ src/widgets/Menu/Menu.tsx
@@ -138,7 +138,6 @@ const Menu: React.FC<NavProps> = ({
           {children}
         </Inner>
       </BodyWrapper>
-      {isMobile && <BottomNav items={links} activeItem={activeItem} activeSubItem={activeSubItem} />}
     </Wrapper>
   );
 };
